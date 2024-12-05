30679f9a20b2e8162e85a4020aa7154e4782351d
gl0p <96223996+gl0p@users.noreply.github.com>
2024-12-05 18:50:25 -0500

    Update manifest_json.h

--- Arduino/manifest_json.h
+++ Arduino/manifest_json.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the open-clock project.
+ * 
+ * Copyright (C) 2024 gl0p
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 unsigned char manifest_json[] = {
   0x7b, 0x0a, 0x20, 0x20, 0x22, 0x73, 0x68, 0x6f, 0x72, 0x74, 0x5f, 0x6e,
   0x61, 0x6d, 0x65, 0x22, 0x3a, 0x20, 0x22, 0x43, 0x6c, 0x6f, 0x63, 0x6b,
