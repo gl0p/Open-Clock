24c3cd6b6ad12ffbeec3cc7eab48a7220d41d0f6
gl0p <96223996+gl0p@users.noreply.github.com>
2024-12-05 18:50:09 -0500

    Update service-worker.h

--- Arduino/service-worker.h
+++ Arduino/service-worker.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the open-clock project.
+ * 
+ * Copyright (C) 2024 gl0p
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 unsigned char service_worker_js[] = {
   0x63, 0x6f, 0x6e, 0x73, 0x74, 0x20, 0x43, 0x41, 0x43, 0x48, 0x45, 0x5f,
   0x4e, 0x41, 0x4d, 0x45, 0x20, 0x3d, 0x20, 0x27, 0x63, 0x6c, 0x6f, 0x63,
