a9d79c437fe883e506eedc32486a20a61dd29597
gl0p <96223996+gl0p@users.noreply.github.com>
2024-12-05 18:50:45 -0500

    Update clock_time_bin.h

--- Arduino/clock_time_bin.h
+++ Arduino/clock_time_bin.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the open-clock project.
+ * 
+ * Copyright (C) 2024 gl0p
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <pgmspace.h>
 const unsigned char clock_time_html_gz[] = {
   0x1f, 0x8b, 0x08, 0x08, 0x7a, 0x46, 0x42, 0x67, 0x02, 0x03, 0x63, 0x6c,
